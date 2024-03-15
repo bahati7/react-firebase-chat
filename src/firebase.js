e8e5bc05c84bc35753b53acff548a723ebfbd8ab
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-03-15 14:14:29 +0200

    implement the google Sign In functionalities

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrOo9VNjfg8LJytR44AytcIKixef7GsUU",
+  authDomain: "chat-app-react-c839d.firebaseapp.com",
+  projectId: "chat-app-react-c839d",
+  storageBucket: "chat-app-react-c839d.appspot.com",
+  messagingSenderId: "158006118706",
+  appId: "1:158006118706:web:6c0b45f195eb63c75def2b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
