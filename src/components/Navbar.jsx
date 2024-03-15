d899ec34ba93841fbdd1962ca95c7d053ac00926
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-03-15 14:28:46 +0200

    implement the logout functionnalities

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {auth} from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import SignIn from './SignIn';
+import Logout from './Logout';
 
 const style={
     nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -13,8 +14,7 @@ const Navbar = () => {
   return (
     <div className={style.nav}>
         <h1 className={style.heading}>Chap App</h1>
-        <SignIn/>
-
+        { !user ? (<SignIn />) : (<Logout/>)}
     </div>
   )
 }
